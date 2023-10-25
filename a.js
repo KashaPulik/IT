a9281cdb508880f86b1b2b3b15041c5281c95826
Kulik Pavel <pashakulik58@gmail.com>
2023-10-25 13:00:04 +0700

    aboba2

--- a.js
+++ a.js
@@ -1,5 +1,5 @@
 document.querySelector('.header__arrow').addEventListener('click', function () {
-    const target = document.querySelector('.catalog');
+    const target = document.querySelector('.anchor');
 
     target.scrollIntoView({
         behavior: 'smooth'
