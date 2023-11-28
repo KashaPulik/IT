9313280262f8bc31ffa4b608fdc8951f78a7977b
Kulik Pavel <pashakulik58@gmail.com>
2023-11-29 00:37:41 +0700

    Make html and css for cart

--- a.js
+++ a.js
@@ -1,7 +1,35 @@
-document.querySelector('button[type="arrow"]').addEventListener('click', function () {
-    const target = document.querySelector('.anchor');
+function smooth_scroll(selector) {
+    const target = document.querySelector(selector);
 
-    target.scrollIntoView({
-        behavior: 'smooth'
-    });
+    if (target) {
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    } else {
+        console.error('Element with selector', selector, 'not found.');
+    }
+}
+
+document.querySelector('button[type="arrow"]').addEventListener('click', function () {
+    smooth_scroll('.anchor');
+});
+
+document.querySelector('button[type="about"]').addEventListener('click', function () {
+    smooth_scroll('.anchor');
+});
+
+document.querySelector('button[type="catalog"]').addEventListener('click', function () {
+    smooth_scroll('.catalog__list');
+});
+
+document.querySelector('button[type="advantages"]').addEventListener('click', function () {
+    smooth_scroll('.advantages');
+});
+
+document.querySelector('button[type="contacts"]').addEventListener('click', function () {
+    smooth_scroll('.basement');
+});
+
+document.querySelector('button[type="buy_duck"]').addEventListener('click', function () {
+    smooth_scroll('.catalog__list');
 });
